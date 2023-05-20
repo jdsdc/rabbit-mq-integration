a60ab79f27f32b75d42ba879ae329fd356c0470f
Johannes Dressie <johannes.dressie@ecoclime.se>
2023-05-20 10:07:30 +0200

    Cleanup

---
+++ src/main/java/dev/coffe/springintegration/rabbitmqintegration/RabbitMqIntegrationApplication.java
@@ -0,0 +1,154 @@
+package dev.coffe.springintegration.rabbitmqintegration;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.integration.amqp.dsl.Amqp;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+
+import java.util.Map;
+import java.util.Set;
+
+import static dev.coffe.springintegration.rabbitmqintegration.RabbitMqIntegrationApplication.*;
+
+@SpringBootApplication
+public class RabbitMqIntegrationApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(RabbitMqIntegrationApplication.class, args);
+        Thread.currentThread().join();
+    }
+
+    final static String BASIC_REQUESTS_NAME = "basic-requests";
+    final static String  INTEGRATION_REQUESTS_NAME = "integration-requests";
+
+    static Map<String, String> payload(String name) {
+        return Map.of("message", "Hello " + name);
+    }
+
+    static void dump(Object payload, Map<String, Object> headers) {
+        System.out.println("-----------------------------------------------");
+        headers.forEach((k, v) -> System.out.println(k + " = " + v));
+        System.out.println(payload);
+    }
+
+}
+
+
+@Configuration
+class RabbitConfiguration {
+    private final ObjectMapper objectMapper;
+
+    public RabbitConfiguration(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    private final TypeReference<Map<String, String>> typeReference = new TypeReference<>() {};
+
+    @Bean
+    ApplicationRunner basicProducer(AmqpTemplate rabbitTemplate) {
+        return args -> {
+            var json = objectMapper.writeValueAsBytes(payload("basics"));
+            var message = MessageBuilder
+                    .withBody(json)
+                    .setHeader(MessageHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                    .build();
+            rabbitTemplate.send(BASIC_REQUESTS_NAME, BASIC_REQUESTS_NAME, message);
+        };
+    }
+
+    @RabbitListener(queues = BASIC_REQUESTS_NAME)
+    public void basicConsumer(@Headers Map<String, Object> headers, @Payload Message payload) throws Exception {
+        var map = this.objectMapper.readValue(payload.getBody(), this.typeReference);
+        dump(map, headers);
+    }
+
+}
+
+@Configuration
+class RabbitIntegrationConfiguration {
+
+    @Bean
+    IntegrationFlow inboundRabbitIntegrationFlow(ConnectionFactory connectionFactory) {
+        return IntegrationFlow
+                .from(Amqp.inboundAdapter(connectionFactory, INTEGRATION_REQUESTS_NAME))
+                .handle((payload, headers) -> {
+                    dump(payload, headers);
+                    return null;
+                })
+                .get();
+    }
+
+    @Bean
+    MessageChannel integrationMessageChannel() {
+        return MessageChannels.direct().get();
+    }
+
+    @Bean
+    IntegrationFlow outboundRabbitIntegrationFlow(AmqpTemplate template) {
+        return IntegrationFlow
+                .from(integrationMessageChannel())
+                .handle(Amqp.outboundAdapter(template)
+                        .exchangeName(INTEGRATION_REQUESTS_NAME)
+                        .routingKey(INTEGRATION_REQUESTS_NAME)
+                        .get())
+                .get();
+    }
+
+    @Bean
+    ApplicationRunner integrationRunner() {
+        return args -> {
+            integrationMessageChannel()
+                    .send(org.springframework.messaging.support.MessageBuilder.withPayload(payload("Integration"))
+                            .build());
+        };
+    }
+
+}
+
+@Configuration
+class InfrastructureConfiguration {
+
+    @Bean
+    InitializingBean initializeRabbitMqBroker(AmqpAdmin admin) {
+        return () -> Set
+                .of(BASIC_REQUESTS_NAME, INTEGRATION_REQUESTS_NAME)
+                .forEach(name -> define(admin, name));
+    }
+
+    private static Queue define(AmqpAdmin admin,
+                                String name) {
+        var q = QueueBuilder
+                .durable(name)
+                .build();
+        var e = ExchangeBuilder
+                .topicExchange(name)
+                .build();
+        var b = BindingBuilder
+                .bind(q)
+                .to(e)
+                .with(name)
+                .noargs();
+        admin.declareQueue(q);
+        admin.declareExchange(e);
+        admin.declareBinding(b);
+        return q;
+    }
+
+}
+
+
---
+++ src/test/java/dev/coffe/springintegration/rabbitmqintegration/RabbitMqIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.coffe.springintegration.rabbitmqintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
